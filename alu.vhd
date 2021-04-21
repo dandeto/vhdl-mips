0dbff612dec6e27faf94790ff5e6f8a284f8d83c
dandeto <danielmichaelmills@gmail.com>
2021-04-20 19:12:48 -0500

    Sorry, Austin

--- alu.vhd
+++ alu.vhd
@@ -24,7 +24,7 @@ architecture behave of alu is begin
 					result <= "11111111111111111111111111111111"; -- set on less than
 					else result <= "00000000000000000000000000000000"; 
 				end if;
-			when "0110" => result <= not (opA or opB); -- branch if equal
+			when "1100" => result <= not (opA or opB); -- branch if equal
 			when others => result <= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 		end case;
 	end process;
